7018268e9a55c0ea15248ed835874072bbe66eb4
alex23job <alex23job@yandex.ru>
2023-12-09 21:15:42 +0300

    Add Actions Window

---
+++ BankWpfApp/ActionsWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BankWpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для ActionsWindow.xaml
+    /// </summary>
+    public partial class ActionsWindow : Window
+    {
+        public ActionsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDecClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnIncClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnTransferClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnPayClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnCMB1SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void OnCMB2SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
